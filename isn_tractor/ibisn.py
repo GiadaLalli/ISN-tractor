4bed28c36dc4cb5e255a9d247af2e11a0550df30
Giada Lalli <45395344+GiadaLalli@users.noreply.github.com>
2023-01-13 14:45:48 +0100

    Add a "dot" (for dot product) metric to be used by compute_isn
    Add the metric code and update typing.
    This constitutes a change to the public API so is a major version bump.
    
    Co-authored-by: James Collier <james.collier@vib.be>

--- isn_tractor/ibisn.py
+++ isn_tractor/ibisn.py
@@ -231,6 +231,10 @@ def __compute_metric(X, Y, method, pool):  # pylint: disable=C0103
         scores = allel.rogers_huff_r_between(X.T, Y.T)  # LD r score
         scores = np.square(scores)  # LD r^2 score
         score = __pooling(scores, pool)
+    elif method == "dot":  # dot product
+        # pylint: disable=E1101
+        scores = t.matmul(X.T, Y)
+        score = __pooling(scores, pool)
     else:
         raise ValueError("Wrong input for metric!")
     return score
@@ -260,7 +264,11 @@ def compute_isn(
     interact_snp,
     interact_gene,
     metric: Union[
-        Literal["pearson"], Literal["spearman"], Literal["mutual_info"], Literal["LD"]
+        Literal["pearson"],
+        Literal["spearman"],
+        Literal["mutual_info"],
+        Literal["LD"],
+        Literal["dot"],
     ],
     pool: Union[Literal["max"], Literal["avg"], Literal["average"]],
 ):
