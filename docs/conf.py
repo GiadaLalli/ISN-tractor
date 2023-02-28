ea923a14d7a384dae7f5e2bd502598763ac4c25f
Giada Lalli <45395344+GiadaLalli@users.noreply.github.com>
2023-02-28 11:31:26 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-root_doc = 'index2.rst'
+root_doc = 'index'
 
 
 # -- Options for HTML output -------------------------------------------------
