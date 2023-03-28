d8b1078bf242d2f96a230ce2d20c26f4997c83e0
Giada Lalli <giadalalli@gmail.com>
2023-03-28 16:59:35 +0200

    working on Pearson

--- isn_tractor/ibisn.py
+++ isn_tractor/ibisn.py
@@ -226,9 +226,10 @@ def map_interaction(
 def __pearson_metric(first: t.Tensor, second: t.Tensor) -> t.Tensor:
     if first.dim() == 1 and second.dim() == 1:
         combined = t.stack([first, second], dim=1)
+	return t.corrcoef(combined.T)[0,1]
     else:
         combined = t.cat([first, second], dim=1)
-    return t.corrcoef(combined.T)[: first.shape[1], first.shape[1] :]
+    	return t.corrcoef(combined.T)[: first.shape[1], first.shape[1] :]
 
 def __spearman_metric(first, second):
     if (first.dim(), second.dim()) == (1, 1):
