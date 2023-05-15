79ea2fb318d7805441b8c687c8e2b67e868333af
James Collier <james.collier@vib.be>
2023-05-15 11:13:18 +0200

    Add CI benchmarking of dense_isn

---
+++ test/test_benchmark.py
@@ -0,0 +1,17 @@
+from pandas import DataFrame
+from numpy.random import uniform
+
+from isn_tractor.ibisn import dense_isn
+
+
+def continuous(n_individuals: int, m_genes: int) -> DataFrame:
+    return DataFrame(
+        uniform(-100, 100, size=(n_individuals, m_genes)),
+        index=["sample_" + str(i) for i in range(n_individuals)],
+        columns=["mapped_feature_" + str(i) for i in range(m_genes)],
+    )
+
+
+def test_dense_isn(benchmark):
+    data = continuous(200, 1000)
+    benchmark.pedantic(dense_isn, args=(data,), rounds=20, iterations=3)
