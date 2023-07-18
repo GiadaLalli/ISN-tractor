002afe0dde9f252b6e31403de07864e95d17263f
James Collier <james.collier@vib.be>
2023-07-18 14:12:59 +0200

    Formatting

--- visualisation/filtration_curve.py
+++ visualisation/filtration_curve.py
@@ -10,7 +10,9 @@ import matplotlib.pyplot as plt
 import isn_tractor.ibisn as it
 
 
-def analyze_network_data(df, abs_val=True, thr_values=None, label_column="label", output=None):
+def analyze_network_data(
+    df, abs_val=True, thr_values=None, label_column="label", output=None
+):
     """Preprocess data by label."""
     if thr_values is None:
         thr_values = np.arange(-4, 4, 0.03)
@@ -98,6 +100,7 @@ def analyze_network_data(df, abs_val=True, thr_values=None, label_column="label"
     else:
         plt.savefig(output)
 
+
 if __name__ == "__main__":
     # Load in data
     expr_filename = sys.argv[1]
@@ -106,9 +109,9 @@ if __name__ == "__main__":
     output = sys.argv[4] if sys.argv[4] != "-" else None
     # expr = read_r(expr_filename)[None]
     # expr = np.load(expr_filename)
-    expr = pd.read_csv(expr_filename, sep=' ', index_col=0)
-    #expr = pd.DataFrame(expr, columns=['gene' + str(i+1) for i in range(expr.shape[1])])
-    clinic = pd.read_csv(clinic_filename, sep=' ')
+    expr = pd.read_csv(expr_filename, sep=" ", index_col=0)
+    # expr = pd.DataFrame(expr, columns=['gene' + str(i+1) for i in range(expr.shape[1])])
+    clinic = pd.read_csv(clinic_filename, sep=" ")
     print(expr.shape)
     print(clinic.shape)
     print(expr.head())
@@ -124,8 +127,10 @@ if __name__ == "__main__":
     ISNs = pd.DataFrame(it.dense_isn(expr)).astype(float)
     a = np.repeat(expr.columns, expr.shape[1])
     b = np.tile(expr.columns, expr.shape[1])
-    ISNs.columns = [a[i]+'_'+b[i] for i in range(expr.shape[1]**2)]
+    ISNs.columns = [a[i] + "_" + b[i] for i in range(expr.shape[1] ** 2)]
 
     # Filtration curve
-    df = pd.concat([ISNs, clinic['mets']], axis=1)
-    analyze_network_data(df.iloc[:20], label_column='mets', abs_val=False, output=output)
+    df = pd.concat([ISNs, clinic["mets"]], axis=1)
+    analyze_network_data(
+        df.iloc[:20], label_column="mets", abs_val=False, output=output
+    )
