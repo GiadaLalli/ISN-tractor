e0e06b5e931b0ef614c12820a523187d15e6803d
James Collier <james.collier@vib.be>
2023-07-31 20:22:29 +0200

    Generate a filtration curve plot using data from the Lioness paper.
    Completed with the help of @FedericoMelograna.

--- visualisation/filtration_curve.py
+++ visualisation/filtration_curve.py
@@ -4,96 +4,85 @@ import sys
 
 import pandas as pd
 import numpy as np
-import networkx as nx
+from networkx import Graph, from_pandas_edgelist, to_numpy_array, from_numpy_array
 import matplotlib.pyplot as plt
 
-import isn_tractor.ibisn as it
+from isn_tractor.ibisn import dense_isn
 
 
-def analyze_network_data(
-    df, abs_val=True, thr_values=None, label_column="label", output=None
-):
-    """Preprocess data by label."""
-    if thr_values is None:
-        thr_values = np.arange(-4, 4, 0.03)
+def preprocess(data, label_column: str):
+    """Seperate data into cases and controls and add necessary columns."""
+    data.index.name = None
 
-    labels = np.sort(df[label_column].unique())
-    print(labels)
+    controls = (
+        data[data[label_column] == 0].drop([label_column], axis=1).T.reset_index()
+    )
+    cases = data[data[label_column] == 1].drop([label_column], axis=1).T.reset_index()
+    controls[["N1", "N2"]] = controls["index"].str.split("_", expand=True)
+    controls = controls.drop(["index"], axis=1)
 
-    df.index.name = None
+    cases[["N1", "N2"]] = cases["index"].str.split("_", expand=True)
+    cases = cases.drop(["index"], axis=1)
 
-    data = []
-    for label in labels:
-        data_label = df[df[label_column] == label]
+    return controls, cases
 
-        data_label.drop([label_column], axis=1, inplace=True)
 
-        data_label = data_label.T.reset_index()
-
-        data_label[["N1", "N2"]] = data_label["index"].str.split("_", expand=True)
-        data_label.drop(["index"], axis=1, inplace=True)
-
-        data.append(data_label)
-
-    data_list = [{} for i in labels]
-    for label in range(len(labels)):
-        for i in range(data[label].shape[1] - 2):
-            temp = (
-                data[label]
-                .iloc[:, [data[label].shape[1] - 2, data[label].shape[1] - 1, i]]
-                .copy()
-            )
-            if abs_val:
-                temp.iloc[:, 2] = np.abs(temp.iloc[:, 2])
-            aa = temp.columns[2]
-            temp.columns = ["E1", "E2", "Weight"]
-            mygraph = nx.from_pandas_edgelist(temp, "E1", "E2", "Weight")
-            gg = nx.to_numpy_array(mygraph, weight="Weight")
-            data_list[label][aa] = gg
-
-    FCs = []
-    for i in range(len(labels)):
-        FC = np.zeros((len(data_list[i]), len(thr_values)))
-        FCs.append(FC)
-
-    for label in range(len(labels)):
-        for indv in range(len(data_list[label])):
-            nm_l = list(data_list[label].keys())[indv]
-            Adj = data_list[label][nm_l]
-            for i, thr_value in enumerate(thr_values):
-                Adj_bin = np.where((Adj > thr_value) & (Adj != 0), 1, 0)
-                g = nx.from_numpy_array(Adj_bin, create_using=nx.Graph)
-                FCs[label][indv, i] = g.number_of_edges()
-
-    df = []
-    for i in range(len(labels)):
-        df.append(
-            pd.DataFrame(
-                {
-                    "Thr": thr_values,
-                    "Mean": np.mean(FCs[i], axis=0),
-                    "sd": np.std(FCs[i], axis=0),
-                    "cl": ["Cluster " + str(i + 1)] * len(thr_values),
-                }
-            )
+def graphs(data):
+    """Compute graphs from ISNs."""
+    data_list = {}
+    for i in range(data.shape[1] - 2):
+        temp = data.iloc[:, [data.shape[1] - 2, data.shape[1] - 1, i]].copy()
+        key = temp.columns[2]
+        temp.columns = ["E1", "E2", "Weight"]
+        data_list[key] = to_numpy_array(
+            from_pandas_edgelist(temp, "E1", "E2", "Weight"), weight="Weight"
         )
+
+    return data_list
+
+
+def calculate_filtration_curve(data_list, thr_values):
+    """Apply the stat (number of graph edges) to the computed ISNs."""
+    curve = np.zeros((len(data_list), len(thr_values)))
+    for a, (individual, Adj) in enumerate(data_list.items()):
+        for i, thr_value in enumerate(thr_values):
+            curve[a, i] = from_numpy_array(
+                np.where((Adj > thr_value) & (Adj != 0), 1, 0), create_using=Graph
+            ).number_of_edges()
+
+    return curve
+
+
+def plot_filtration_curve(df, label_column="label", output=None):
+    """Plot labeled data."""
+    thr_values = np.arange(-4, 4, 0.03)
+
+    controls_data, cases_data = preprocess(df, label_column)
+    controls_FC = calculate_filtration_curve(graphs(controls_data), thr_values)
+    cases_FC = calculate_filtration_curve(graphs(cases_data), thr_values)
 
     plt.figure(figsize=(12, 10))
-    for i in range(len(labels)):
-        plt.errorbar(
-            df[i]["Thr"],
-            df[i]["Mean"],
-            yerr=df[i]["sd"],
-            elinewidth=0.5,
-            label="Cluster " + str(i + 1),
-        )
-
+    labels = {"Cases": "red", "Controls": "blue"}
+    plt.errorbar(
+        thr_values,
+        np.mean(controls_FC, axis=0),
+        yerr=np.std(controls_FC, axis=0),
+        elinewidth=0.5,
+    )
+    plt.errorbar(
+        thr_values,
+        np.mean(cases_FC, axis=0),
+        yerr=np.std(cases_FC, axis=0),
+        elinewidth=0.5,
+        color="red",
+    )
     plt.xlabel("Threshold Values", fontsize=20)
-    plt.ylabel("Graph Statistic: Strength", fontsize=20)
-
-    leg = plt.legend(loc="upper left", fontsize=15)
-    for line in leg.get_lines():
-        line.set_linewidth(10)
+    plt.ylabel("Graph Statistic: N Edges", fontsize=20)
+    handles = [
+        plt.Line2D([], [], color=labels[label], marker="o", linestyle="-")
+        for label in labels
+    ]
+    plt.legend(handles, labels.keys(), loc="upper right", fontsize=15)
 
     if output is None:
         plt.show()
@@ -101,38 +90,162 @@ def analyze_network_data(
         plt.savefig(output)
 
 
+def find_filter_edges(expr, sig):
+    """Find the indexes of the columns we want to keep."""
+    columns = np.asarray(
+        [
+            f"{a}_{b}"
+            for (a, b) in zip(
+                np.repeat(expr.columns, expr.shape[1]),
+                np.tile(expr.columns, expr.shape[1]),
+            )
+        ]
+    )
+    sorter = np.argsort(columns)
+    return sorter[np.searchsorted(columns, sig, sorter=sorter)]
+
+
+TOP_EDGES = [
+    "BGLAP_MMP11",
+    "MFAP4_FKSG30",
+    "MFAP4_CSPG2",
+    "CSPG2_MXRA5",
+    "COL8A1_H19",
+    "PTHR1_DSPG3",
+    "CXCL10_HOMER2",
+    "CSPG2_DDIT4L",
+    "TMSL8_SRPX",
+    "BGLAP_HLA-DQB1",
+    "PRSS35_HLA-DQB1",
+    "AEBP1_SERPINE2",
+    "FBLN2_CD248",
+    "SULF1_CD248",
+    "PHGDH_CDKN2A",
+    "BGLAP_GZMA",
+    "MFI2_GZMA",
+    "RPS4Y1_SLC2A3",
+    "FOS_SCIN",
+    "CXCL9_SCIN",
+    "SRPX_SCIN",
+    "ENPP2_IFIT3",
+    "TMSL8_SNAI2",
+    "SULF1_SNAI2",
+    "SNAI2_VWF",
+    "ADM_LEPREL1",
+    "SCRG1_AXL",
+    "PLTP_IGFBP3",
+    "MFAP4_SBDS",
+    "SOST_STAT1",
+    "IBSP_STAT1",
+    "MEPE_STAT1",
+    "IFITM5_STAT1",
+    "KSP37_STAT1",
+    "CDH15_STAT1",
+    "PTHR1_STAT1",
+    "TMEM119_STAT1",
+    "SP7_STAT1",
+    "MFI2_STAT1",
+    "EFHD1_STAT1",
+    "CCDC3_STAT1",
+    "SEPP1_STAT1",
+    "CD36_STAT1",
+    "HOMER2_STAT1",
+    "GYG2_STAT1",
+    "ENPP2_STAT1",
+    "ID3_STAT1",
+    "SCIN_STAT1",
+    "FOSB_SLC29A4",
+    "SRPX_TNFSF10",
+]
+
+TOP_NODES = [
+    "BGLAP",
+    "MMP11",
+    "MFAP4",
+    "FKSG30",
+    "CSPG2",
+    "MXRA5",
+    "COL8A1",
+    "H19",
+    "PTHR1",
+    "DSPG3",
+    "CXCL10",
+    "HOMER2",
+    "DDIT4L",
+    "TMSL8",
+    "SRPX",
+    "HLA-DQB1",
+    "PRSS35",
+    "AEBP1",
+    "SERPINE2",
+    "FBLN2",
+    "CD248",
+    "SULF1",
+    "PHGDH",
+    "CDKN2A",
+    "GZMA",
+    "MFI2",
+    "RPS4Y1",
+    "SLC2A3",
+    "FOS",
+    "SCIN",
+    "CXCL9",
+    "ENPP2",
+    "IFIT3",
+    "SNAI2",
+    "VWF",
+    "ADM",
+    "LEPREL1",
+    "SCRG1",
+    "AXL",
+    "PLTP",
+    "IGFBP3",
+    "SBDS",
+    "SOST",
+    "STAT1",
+    "IBSP",
+    "MEPE",
+    "IFITM5",
+    "KSP37",
+    "CDH15",
+    "TMEM119",
+    "SP7",
+    "EFHD1",
+    "CCDC3",
+    "SEPP1",
+    "CD36",
+    "GYG2",
+    "ID3",
+    "FOSB",
+    "SLC29A4",
+    "TNFSF10",
+]
+
 if __name__ == "__main__":
     # Load in data
     expr_filename = sys.argv[1]
     clinic_filename = sys.argv[2]
-    subset = int(sys.argv[3])
-    output = sys.argv[4] if sys.argv[4] != "-" else None
-    # expr = read_r(expr_filename)[None]
-    # expr = np.load(expr_filename)
-    expr = pd.read_csv(expr_filename, sep=" ", index_col=0).T
-    # expr = pd.DataFrame(expr, columns=['gene' + str(i+1) for i in range(expr.shape[1])])
-    clinic = pd.read_csv(clinic_filename, sep=" ", index_col=0)
-    clinic = clinic.replace({"yes": 1, "no": 0})
-    print(expr.shape)
-    print(clinic.shape)
+    output = sys.argv[3] if sys.argv[3] != "-" else None
 
-    # Select the most variable features
-    # variance = np.argsort(np.var(expr, axis=0))[::-1]
-    # expr = expr.iloc[:subset, variance[:subset]]
-    clinic = clinic.iloc[:subset]
+    sig = np.asarray(TOP_EDGES)
+    nodes = np.asarray(TOP_NODES)
+    expr = pd.read_csv(expr_filename).loc[:, nodes]
+    clinic = pd.read_csv(clinic_filename)
+    clinic["mets"] = clinic["mets"].replace({"yes": 1, "no": 0})
 
-    # ISN computation
-    # ISNs = pd.DataFrame(it.dense_isn(expr)).astype(float)
-    # a = np.repeat(expr.columns, expr.shape[1])
-    # b = np.tile(expr.columns, expr.shape[1])
-    # ISNs.columns = [a[i] + "_" + b[i] for i in range(expr.shape[1] ** 2)]
-    ISNs = expr.iloc[:subset, :subset]
-    print(ISNs.shape)
-    print(clinic.shape)
+    filter_edges = find_filter_edges(expr, sig)
+
+    # This produces a very large dataframe
+    #  we reduce it to what we need before storing it
+    #  by filtering out only the edges we're interested in.
+    ISNs = pd.DataFrame(
+        [edge.numpy()[filter_edges] for edge in dense_isn(expr)],
+        columns=sig,
+        index=clinic["sample"],
+    ).loc[:, sig]
 
     # Filtration curve
-    df = pd.concat([ISNs, clinic["mets"]], axis=1)
-    print(df.shape)
-    analyze_network_data(
-        df.iloc[:20], label_column="mets", abs_val=False, output=output
-    )
+    df = pd.concat([ISNs, clinic.rename(index=clinic["sample"])["mets"]], axis=1)
+    df.to_csv("isns.csv")
+
+    plot_filtration_curve(df, label_column="mets", output=output)
